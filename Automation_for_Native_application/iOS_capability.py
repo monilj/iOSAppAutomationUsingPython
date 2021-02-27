507960840f4c62c56b8a7538c97635bfab88fa2c
localadmin <localadmin@MONILJ-MAC.local>
2021-02-28 01:15:17 +0530

    Added capabilities to launch ios native application

---
+++ Automation_for_Native_application/iOS_capability.py
@@ -0,0 +1,17 @@
+from appium import webdriver
+import unittest
+
+
+def des_cap_calendar_app():
+    desired_caps = dict(
+        platformName='iOS',
+        platformVersion='12.4.5',
+        automationName='XCUITest',
+        udid='120a5ede58d4aeab0f4641213dac1f913b4ec6e0',
+        deviceName='Qualitia',
+        bundleId='com.apple.MobileSMS')
+    driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+    return driver
+
+
+des_cap_calendar_app()
